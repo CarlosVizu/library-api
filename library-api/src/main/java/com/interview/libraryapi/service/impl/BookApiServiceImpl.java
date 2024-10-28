ece4975cd22e971ae648baf3127e51b99efec1e8
carlos_vizu <kaduvizu@hotmail.com>
2024-10-28 09:57:49 -0300

    fix: Correção para salvar o Livro pela estrutura de pesquisa única a partir do ID

--- library-api/src/main/java/com/interview/libraryapi/service/impl/BookApiServiceImpl.java
+++ library-api/src/main/java/com/interview/libraryapi/service/impl/BookApiServiceImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interview.libraryapi.data.dto.v1.LivroDTO;
+import com.interview.libraryapi.exceptions.ResourceNotFoundException;
 import com.interview.libraryapi.model.Livro;
 import com.interview.libraryapi.repositories.LivroRepository;
 import com.interview.libraryapi.service.BookApiService;
@@ -59,6 +60,7 @@ public class BookApiServiceImpl implements BookApiService {
         builder.url(PropertiesReader.getProperty("API_URL") + id + "&key=" + PropertiesReader.getProperty("API_KEY"));
         Request request = builder.build();
         Response response = null;
+
         try {
             response = okHttpClient.newCall(request).execute();
             json = response.body().string();
@@ -72,20 +74,22 @@ public class BookApiServiceImpl implements BookApiService {
         Livro livro = new Livro();
         try {
             JsonNode node = mapper.readTree(json);
-            livro.setTitulo(node.get("items").get(0).get("volumeInfo").get("title").asText());
-            livro.setAutor(node.get("items").get(0).get("volumeInfo").get("authors").get(0).asText());
-            DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-            Date date = formatter.parse(node.get("items").get(0).get("volumeInfo").get("publishedDate").textValue());
+            livro.setTitulo(node.get("volumeInfo").get("title").asText());
+            livro.setAutor(node.get("volumeInfo").get("authors").get(0).asText());
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = formatter.parse(node.get("volumeInfo").get("publishedDate").textValue());
             livro.setDataPublicacao(date);
-            livro.setIsbn(node.get("items").get(0).get("volumeInfo").get("industryIdentifiers").get(0).get("identifier").textValue());
-            livro.setCategoria(node.get("items").get(0).get("volumeInfo").get("categories").get(0).asText());
+            livro.setIsbn(node.get("volumeInfo").get("industryIdentifiers").get(0).get("identifier").textValue());
+            livro.setCategoria(node.get("volumeInfo").get("categories").get(0).asText());
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
         } catch (ParseException e) {
             e.printStackTrace();
+        } catch(NullPointerException e) {
+            throw new ResourceNotFoundException("Não foi possível localizar o livro solicitado: " + json);
         }
-
+        
         Livro livroSalvo = repository.save(livro);
         return new LivroDTO(livroSalvo);
     }
